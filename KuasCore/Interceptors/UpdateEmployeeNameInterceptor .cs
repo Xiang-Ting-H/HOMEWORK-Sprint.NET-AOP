dfd1672948a4a19df79ecce7e994bf6d6a8745fb
xiangting_hong <ts00544040@gmail.com>
2015-04-17 16:56:40 +0800

    goodplayer

---
+++ KuasCore/Interceptors/UpdateEmployeeNameInterceptor .cs
@@ -0,0 +1,29 @@
+﻿using AopAlliance.Intercept;
+using KuasCore.Models;
+using System;
+using System.Diagnostics;
+
+namespace KuasCore.Interceptors
+{
+    class UpdateEmployeeNameInterceptor : IMethodInterceptor
+    {
+
+        public object Invoke(IMethodInvocation invocation)
+        {
+            Console.WriteLine("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+            Debug.Print("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+
+            object result = invocation.Proceed();
+
+            if (result is Employee)
+            {
+                Employee employee = (Employee)result;
+                employee.Name = employee + "123";
+                result = employee;
+
+            }
+
+            return result;
+        }
+    }
+}
